92f75db1ef6c00ae1793657f2340ddc2fea3b979
5878794@qq.com <xf771026aa>
2019-05-15 16:43:10 +0800

    save page ok

--- myTravel/miniprogram/lib/gdMap/gd.js
+++ myTravel/miniprogram/lib/gdMap/gd.js
@@ -57,5 +57,24 @@ module.exports = {
 				}
 			})
 		})
+	},
+
+	//获取2个经纬度之间的直线距离
+	get2PointLength(longitude1, latitude1, longitude2, latitude2){
+		let lat1 = latitude1*Math.PI/180,
+			lat2 = latitude2 * Math.PI / 180,
+			lon1 = longitude1 * Math.PI / 180,
+			lon2 = longitude2 * Math.PI / 180,
+			a = lat1 - lat2,
+			b = lon1 - lon2,
+			s = 2 * Math.asin(Math.sqrt(Math.pow(Math.sin(a / 2), 2) + Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(b / 2), 2)));
+
+		s = s * 6378137.0; //弧长乘地球半径（半径为米）
+		//精确距离的数值 米
+		s = Math.round(s);
+
+
+		return s;
 	}
+
 };
--- myTravel/miniprogram/pages/list/list.js
+++ myTravel/miniprogram/pages/list/list.js
@@ -1,5 +1,7 @@
 const regeneratorRuntime = require('../../lib/regeneratorRuntime.js'),
     wxApp = require('../../lib/all.js'),
+    setInputVal = require('../../lib/wxPublish/getInputVal'),
+    gd = require('../../lib/gdMap/gd.js'),
     sys = require('../../lib/sys/getUserInfo.js'),
     app = getApp();
 
@@ -13,42 +15,139 @@ Page({
      * 页面的初始数据
      */
     data: {
-        class_icon:'',
-        class_btn:'',
-        icon_img:'../../images/user-unlogin.png',
-        logged:false,
-        userInfo:{}
-    },
-    onGetUserInfo(e){
-        if (!this.data.logged && e.detail.userInfo) {
-            this.setData({
-                logged: true,
-                icon_img: e.detail.userInfo.avatarUrl,
-                userInfo: e.detail.userInfo,
-                class_icon:'show_icon',
-                class_btn:'show_btn'
-            });
-            app.globalData.userInfo = e.detail.userInfo;
-        }
-    },
-    async login(){
-        if(!this.data.logged){return;}
+        //原始数据
+        list:[],
+        //显示用数据
+        listData:[],
+        //我的位置
+        myLocation:{},
+        locationGetTime:0,
 
-        let openId = await sys.getOpenId().catch(rs=>{
-            wxApp.alert(rs);
+        search:''
+    },
+
+    setInputVal:setInputVal,
+
+    async getListData(searchText){
+        const db = wx.cloud.database();
+        const address = db.collection('address');
+        const _ = db.command;
+
+        // let data = await address.where({
+        //     name: {
+        //         $regex: '.*'+searchText+'.*'
+        //     }
+        // }).get();
+
+        //TODO 分页  下拉加载  刷新
+        let data = await address.where(_.or([
+            {name: {$regex: '.*'+searchText+'.*'}},
+            {address:{$regex: '.*'+searchText+'.*'}},
+            {cityname:{$regex: '.*'+searchText+'.*'}},
+            {pname:{$regex: '.*'+searchText+'.*'}}
+        ]))
+            .orderBy('pname','asc')
+            .limit(20)
+            .skip(0)
+            .get();
+
+
+        data = data.data || [];
+
+        data.map(rs=>{
+           rs.zxjl = '计算中...';
         });
 
-        app.globalData.openId = openId;
-
-
-        wxApp.openUrl('./list/list');
+        this.setData({
+            list:data,
+            listData:data
+        });
+        console.log(data);
     },
 
+    getMyLocation(){
+        let _this = this;
+
+        return new Promise(success=>{
+            let time = new Date().getTime();
+
+            //10分钟获取一次定位
+            if(time - this.data.locationGetTime < 600000){
+                success();
+            }else{
+                wx.getLocation({
+                    type: 'gcj02',
+                    altitude:'true',
+                    success(res) {
+                        _this.setData({
+                            myLocation:res,
+                            locationGetTime:new Date().getTime()
+                        });
+                        success();
+                    }
+                })
+            }
+        });
+    },
+
+
+    refreshListJl(){
+        let myLocation = this.data.myLocation,
+            lon = myLocation.longitude,
+            lat = myLocation.latitude,
+            data = this.data.list;
+
+        data.map(rs=>{
+            let location = rs.location.split(','),
+                lon1 = location[0],
+                lon2 = location[1],
+                jl = gd.get2PointLength(lon,lat,lon1,lon2);
+
+            jl = (jl/1000).toFixed(1);
+
+            rs.zxjl = jl;
+            console.log(jl)
+        });
+
+        //对象 内存地址相同 不需要更新正在使用的数据
+        this.setData({
+            list:data
+        })
+    },
+
+    searchVal(){
+        let val = this.data.search;
+
+        this.init(val).then(rs=>{
+
+        }).catch(e=>{
+            wxApp.alert(e);
+        })
+    },
+
+
+    async init(searchText){
+        searchText = searchText || '';
+
+        await this.getListData(searchText);
+        await this.getMyLocation();
+        this.refreshListJl();
+
+        this.setData({
+            listData:this.data.list
+        })
+
+
+    },
     /**
      * 生命周期函数--监听页面加载
      */
-    onLoad: function (options) {
+    onLoad:function (options) {
+        this.init().then(rs=>{
 
+        }).catch(e=>{
+            wxApp.alert(e);
+        })
     },
 
     /**
