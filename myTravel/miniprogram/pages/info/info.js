2cfb6b8038ffcb9cb8539df0e7956db490ebee0a
5878794@qq.com <xf771026aa>
2019-05-17 11:30:56 +0800

    info page ok

--- myTravel/miniprogram/pages/info/info.js
+++ myTravel/miniprogram/pages/info/info.js
@@ -137,7 +137,7 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-        let id = options.id || 'B03440N0U0';
+        let id = options.id;
         if(!id){
             wxApp.goBack();
             return;
